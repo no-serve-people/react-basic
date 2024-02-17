6fae9dc281399c0f15a7bc90bc0d0b6da9759bb8
no-serve-people <ixingjue@qq.com>
2024-02-17 13:47:49 +0800

    事件绑定

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 function App() {
-    const articleType=1
-
-    function getArticleTem(){
-        if (articleType===0){
-            return <div>我是无图文章</div>
-        }else if( articleType===1){
-            return <div>我是单图文章</div>
-        }else {
-            return <div>我是三图模式</div>
-        }
+    // 传递自定义参数 而且需要事件对象e
+    const handleClick=(name,e)=>{
+        console.log('button被点击了',name,e)
     }
   return (
     <div className="App">
-        {/*调用函数渲染不同的模板*/}
-        {getArticleTem()}
+        <button onClick={(e)=>handleClick('jack',e)}>click me</button>
     </div>
   );
 }
