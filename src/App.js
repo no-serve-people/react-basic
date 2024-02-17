53993e08535dd11cfa91f0f7a0dc5986ef532b26
no-serve-people <ixingjue@qq.com>
2024-02-17 11:05:19 +0800

    jsx实现列表渲染

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 function App() {
-    const count =100
-
-    function getName(){
-        return 'hhh'
-    }
+  const list=[
+      {id:1001,name:'vue'},
+      {id:1002,name:'react'},
+      {id:1003,name:'angular'}
+  ]
   return (
     <div className="App">
-      this is react
-        {/* 使用引号传递字符串*/}
-        {' this is message'}
-        {/*识别Js变量*/}
-        {count }
-        {/*函数调用*/}
-        {getName()}
-        {/*方法调用*/}
-        {
-            new Date().getDate() +'\n'
-        }
-        {/*使用js对象*/}
-        <div style={{color:'red'}}>this is div</div>
+        {/*渲染列表*/}
+        <ul>
+            {list.map(item=><li key={item.id}>{item.name}</li>)}
+        </ul>
     </div>
   );
 }
